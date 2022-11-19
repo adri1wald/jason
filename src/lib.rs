012303d8ed6adfae02184d77356cb53fcc2bb119
Adrien Wald <adrien@genei.io>
2022-11-19 01:59:00 +0000

    feat: new tokenizer impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
-pub mod parser;
 pub mod ast;
+pub mod parser;
+pub mod tokenizer;
 
 #[cfg(test)]
 mod tests {
-    use super::parser::Parser;
-    use super::ast::Node;
+    use super::tokenizer::{tokenize, TokenKind};
 
     #[test]
-    fn parse_int() {
-        let string = "42";
-        let parser = Parser::new(&string);
-        let ast = parser.parse();
-        assert_eq!(ast, Node::Int(42));
+    fn tokenize_array() {
+        let data = " [  1,  2, 3, 4 ] ";
+        let tokens = tokenize(data);
+        if let Ok(tokens) = tokens {
+            let tokens: Vec<_> = tokens.iter().map(|(tok, _, _)| tok).collect();
+            let expected_tokens = vec![
+                &TokenKind::OpenSquare,
+                &TokenKind::Integer(1),
+                &TokenKind::Comma,
+                &TokenKind::Integer(2),
+                &TokenKind::Comma,
+                &TokenKind::Integer(3),
+                &TokenKind::Comma,
+                &TokenKind::Integer(4),
+                &TokenKind::CloseSquare,
+            ];
+            assert_eq!(tokens, expected_tokens);
+        }
     }
 }
